b510051fe0fc9757c0409d3bad13190324e8f3d7
tristan-morris <44310937+tristan-morris@users.noreply.github.com>
2024-06-09 10:52:44 +1000

    Address clippy warnings

--- src/download.rs
+++ src/download.rs
@@ -13,18 +13,21 @@ const BUCKET: &str = "noaa-nexrad-level2";
 
 /// List data files for the specified site and date. This effectively returns an index of data files
 /// which can then be individually downloaded.
+///
+/// # Errors
+/// Will error if the list of files cannot be retrieved.
 pub async fn list_files(site: &str, date: &NaiveDate) -> Result<Vec<FileMetadata>> {
     // Query S3 for objects matching the prefix (i.e. files for the specified site and date)
     let prefix = format!("{}/{}", date.format("%Y/%m/%d"), site);
-    let objects = list_objects(&get_client().await, BUCKET, &prefix)
+    let objects = list_objects(&get_client(), BUCKET, &prefix)
         .await?
-        .expect("should return objects");
+        .unwrap_or_default();
 
     // Pull the returned objects' keys and parse them into metadata
     let metas = objects
         .iter()
-        .map(|object| {
-            let key = object.key().expect("object should have a key");
+        .filter_map(|object| {
+            let key = object.key()?;
 
             // E.g. 2023/04/06/KDMX/KDMX20230406_000215_V06
             //      date_string:    "2023_04_06"
@@ -34,13 +37,12 @@ pub async fn list_files(site: &str, date: &NaiveDate) -> Result<Vec<FileMetadata
             let parts: Vec<&str> = key.split('/').collect();
 
             let date_string = parts[0..=2].join("/");
-            let date = NaiveDate::parse_from_str(&date_string, "%Y/%m/%d")
-                .unwrap_or_else(|_| panic!("file has valid date: \"{}\"", date_string));
+            let date = NaiveDate::parse_from_str(&date_string, "%Y/%m/%d").ok()?;
 
             let site = parts[3];
             let identifier = parts[4..].join("");
 
-            FileMetadata::new(site.to_string(), date, identifier)
+            Some(FileMetadata::new(site.to_string(), date, identifier))
         })
         .collect();
 
@@ -49,13 +51,17 @@ pub async fn list_files(site: &str, date: &NaiveDate) -> Result<Vec<FileMetadata
 
 /// Download a data file specified by its metadata. Returns the downloaded file's encoded contents
 /// which may then need to be decompressed and decoded.
+///
+/// # Errors
+/// Will error if the file cannot be retrieved.
+#[allow(clippy::module_name_repetitions)]
 pub async fn download_file(meta: &FileMetadata) -> Result<Vec<u8>> {
     // Reconstruct the S3 object key from the file's metadata
     let formatted_date = meta.date().format("%Y/%m/%d");
     let key = format!("{}/{}/{}", formatted_date, meta.site(), meta.identifier());
 
     // Download the object from S3
-    download_object(&get_client().await, BUCKET, &key).await
+    download_object(&get_client(), BUCKET, &key).await
 }
 
 /// Downloads an object from S3 and returns only its contents. This will only work for
@@ -75,11 +81,13 @@ async fn list_objects(client: &Client, bucket: &str, prefix: &str) -> Result<Opt
     let operation = client.list_objects_v2().bucket(bucket).prefix(prefix);
 
     let response = operation.send().await?;
-    Ok(response.contents().map(|objects| objects.to_vec()))
+    Ok(response
+        .contents()
+        .map(<[aws_sdk_s3::types::Object]>::to_vec))
 }
 
 /// Creates a new S3 client for a predetermined region.
-async fn get_client() -> Client {
+fn get_client() -> Client {
     Client::from_conf(
         Config::builder()
             .region(Region::from_static(REGION))
--- src/model.rs
+++ src/model.rs
@@ -21,21 +21,25 @@ pub struct VolumeHeaderRecord {
 
 impl VolumeHeaderRecord {
     /// Filename of the archive.
+    #[must_use]
     pub fn filename(&self) -> &[u8; 12] {
         &self.filename
     }
 
     /// Modified Julian date of the file.
+    #[must_use]
     pub fn file_date(&self) -> u32 {
         self.file_date
     }
 
     /// Milliseconds of day since midnight of the file.
+    #[must_use]
     pub fn file_time(&self) -> u32 {
         self.file_time
     }
 
     /// ICAO radar identifier in ASCII.
+    #[must_use]
     pub fn radar_id(&self) -> &[u8; 4] {
         &self.radar_id
     }
@@ -58,46 +62,55 @@ pub struct MessageHeader {
 
 impl MessageHeader {
     /// 12 bytes inserted by RPG Communications Mgr. Ignored.
+    #[must_use]
     pub fn rpg(&self) -> &[u8; 12] {
         &self.rpg
     }
 
     /// Message size for this segment, in halfwords.
+    #[must_use]
     pub fn msg_size(&self) -> u16 {
         self.msg_size
     }
 
     /// RDA Redundant Channel
+    #[must_use]
     pub fn channel(&self) -> u8 {
         self.channel
     }
 
     /// Message type. For example, 31.
+    #[must_use]
     pub fn msg_type(&self) -> u8 {
         self.msg_type
     }
 
     /// Msg seq num = 0 to 7FFF, then roll over to 0.
+    #[must_use]
     pub fn id_seq(&self) -> u16 {
         self.id_seq
     }
 
     /// Modified Julian date from 1/1/70.
+    #[must_use]
     pub fn msg_date(&self) -> u16 {
         self.msg_date
     }
 
     /// Packet generation time in ms past midnight.
+    #[must_use]
     pub fn msg_time(&self) -> u32 {
         self.msg_time
     }
 
     /// Number of segments for this message.
+    #[must_use]
     pub fn num_segs(&self) -> u16 {
         self.num_segs
     }
 
     /// Number of this segment.
+    #[must_use]
     pub fn seg_num(&self) -> u16 {
         self.seg_num
     }
@@ -137,61 +150,72 @@ impl Message31 {
     }
 
     /// The message 31 header.
+    #[must_use]
     pub fn header(&self) -> &Message31Header {
         &self.header
     }
 
     /// The volume data block.
+    #[must_use]
     pub fn volume_data(&self) -> Option<&VolumeData> {
         self.volume_data.as_ref()
     }
 
     /// The elevation data block.
+    #[must_use]
     pub fn elevation_data(&self) -> Option<&ElevationData> {
         self.elevation_data.as_ref()
     }
 
     /// The radial data block.
+    #[must_use]
     pub fn radial_data(&self) -> Option<&RadialData> {
         self.radial_data.as_ref()
     }
 
     /// The reflectivity data block.
+    #[must_use]
     pub fn reflectivity_data(&self) -> Option<&DataMoment> {
         self.reflectivity_data.as_ref()
     }
 
     /// The velocity data block.
+    #[must_use]
     pub fn velocity_data(&self) -> Option<&DataMoment> {
         self.velocity_data.as_ref()
     }
 
     /// The spectrum width data block.
+    #[must_use]
     pub fn sw_data(&self) -> Option<&DataMoment> {
         self.sw_data.as_ref()
     }
 
     /// The differential reflectivity data block.
+    #[must_use]
     pub fn zdr_data(&self) -> Option<&DataMoment> {
         self.zdr_data.as_ref()
     }
 
     /// The differential phase data block.
+    #[must_use]
     pub fn phi_data(&self) -> Option<&DataMoment> {
         self.phi_data.as_ref()
     }
 
     /// The correlation coefficient data block.
+    #[must_use]
     pub fn rho_data(&self) -> Option<&DataMoment> {
         self.rho_data.as_ref()
     }
 
     /// The clutter filter power data block.
+    #[must_use]
     pub fn cfp_data(&self) -> Option<&DataMoment> {
         self.cfp_data.as_ref()
     }
 
-    /// Set data based on DataMoment
+    /// Set data based on `DataMoment`
     pub(crate) fn set_data_moment(&mut self, data_moment: DataMoment) {
         match data_moment.product {
             DataBlockProduct::Reflectivity => self.reflectivity_data = Some(data_moment),
@@ -247,81 +271,97 @@ pub struct Message31Header {
 
 impl Message31Header {
     /// Radar site identifier.
+    #[must_use]
     pub fn radar_id(&self) -> &[u8; 4] {
         &self.radar_id
     }
 
     /// Data collection time in milliseconds past midnight GMT.
+    #[must_use]
     pub fn ray_time(&self) -> u32 {
         self.ray_time
     }
 
     /// Julian date - 2440586.5 (1/01/1970).
+    #[must_use]
     pub fn ray_date(&self) -> u16 {
         self.ray_date
     }
 
     /// Radial number within elevation scan.
+    #[must_use]
     pub fn azm_num(&self) -> u16 {
         self.azm_num
     }
 
     /// Azimuth angle in degrees (0 to 359.956055).
+    #[must_use]
     pub fn azm(&self) -> f32 {
         self.azm
     }
 
     /// 0 = uncompressed, 1 = BZIP2, 2 = zlib.
+    #[must_use]
     pub fn compression_code(&self) -> u8 {
         self.compression_code
     }
 
     /// For word alignment.
+    #[must_use]
     pub fn spare(&self) -> u8 {
         self.spare
     }
 
     /// Radial length in bytes, including data header block.
+    #[must_use]
     pub fn radial_len(&self) -> u16 {
         self.radial_len
     }
 
     /// Azimuthal resolution.
+    #[must_use]
     pub fn azm_res(&self) -> u8 {
         self.azm_res
     }
 
     /// Radial status.
+    #[must_use]
     pub fn radial_status(&self) -> u8 {
         self.radial_status
     }
 
     /// Elevation number.
+    #[must_use]
     pub fn elev_num(&self) -> u8 {
         self.elev_num
     }
 
     /// Sector cut number.
+    #[must_use]
     pub fn sector_cut_num(&self) -> u8 {
         self.sector_cut_num
     }
 
     /// Elevation angle in degrees (-7.0 to 70.0).
+    #[must_use]
     pub fn elev(&self) -> f32 {
         self.elev
     }
 
     /// Radial spot blanking.
+    #[must_use]
     pub fn radial_spot_blanking(&self) -> u8 {
         self.radial_spot_blanking
     }
 
     /// Azimuth indexing mode.
+    #[must_use]
     pub fn azm_indexing_mode(&self) -> u8 {
         self.azm_indexing_mode
     }
 
     /// Data block count.
+    #[must_use]
     pub fn data_block_count(&self) -> u16 {
         self.data_block_count
     }
@@ -336,16 +376,21 @@ pub struct DataBlockHeader {
 }
 
 impl DataBlockHeader {
+    #[must_use]
     pub fn data_block_type(&self) -> &[u8; 1] {
         &self.data_block_type
     }
 
     /// Data name, e.g. "REF", "VEL", etc.
+    #[must_use]
     pub fn data_name(&self) -> &[u8; 3] {
         &self.data_name
     }
 
     /// Data block header name
+    ///
+    /// # Errors
+    /// Will error if the data block product is not recognized.
     pub fn data_block_product(&self) -> Result<DataBlockProduct> {
         Ok(DataBlockProduct::from_str(
             String::from_utf8_lossy(self.data_name()).as_ref(),
@@ -435,62 +480,77 @@ pub struct VolumeData {
 }
 
 impl VolumeData {
+    #[must_use]
     pub fn data_block_header(&self) -> &DataBlockHeader {
         &self.data_block_header
     }
 
+    #[must_use]
     pub fn lrtup(&self) -> u16 {
         self.lrtup
     }
 
+    #[must_use]
     pub fn version_major(&self) -> u8 {
         self.version_major
     }
 
+    #[must_use]
     pub fn version_minor(&self) -> u8 {
         self.version_minor
     }
 
+    #[must_use]
     pub fn lat(&self) -> f32 {
         self.lat
     }
 
+    #[must_use]
     pub fn long(&self) -> f32 {
         self.long
     }
 
+    #[must_use]
     pub fn site_height(&self) -> u16 {
         self.site_height
     }
 
+    #[must_use]
     pub fn feedhorn_height(&self) -> u16 {
         self.feedhorn_height
     }
 
+    #[must_use]
     pub fn calibration_constant(&self) -> f32 {
         self.calibration_constant
     }
 
+    #[must_use]
     pub fn shvtx_power_hor(&self) -> f32 {
         self.shvtx_power_hor
     }
 
+    #[must_use]
     pub fn shvtx_power_ver(&self) -> f32 {
         self.shvtx_power_ver
     }
 
+    #[must_use]
     pub fn system_differential_reflectivity(&self) -> f32 {
         self.system_differential_reflectivity
     }
 
+    #[must_use]
     pub fn initial_system_differential_phase(&self) -> f32 {
         self.initial_system_differential_phase
     }
 
+    #[must_use]
     pub fn volume_coverage_pattern_number(&self) -> u16 {
         self.volume_coverage_pattern_number
     }
 
+    #[must_use]
     pub fn processing_status(&self) -> u16 {
         self.processing_status
     }
@@ -506,21 +566,25 @@ pub struct ElevationData {
 }
 
 impl ElevationData {
+    #[must_use]
     pub fn data_block_header(&self) -> &DataBlockHeader {
         &self.data_block_header
     }
 
     /// Size of data block in bytes
+    #[must_use]
     pub fn lrtup(&self) -> u16 {
         self.lrtup
     }
 
     /// Atmospheric Attenuation Factor
+    #[must_use]
     pub fn atmos(&self) -> &[u8; 2] {
         &self.atmos
     }
 
     /// Scaling constant used by the Signal Processor for this elevation to calculate reflectivity
+    #[must_use]
     pub fn calib_const(&self) -> f32 {
         self.calib_const
     }
@@ -541,40 +605,49 @@ pub struct RadialData {
 }
 
 impl RadialData {
+    #[must_use]
     pub fn data_block_header(&self) -> &DataBlockHeader {
         &self.data_block_header
     }
 
     /// Size of data block in bytes
+    #[must_use]
     pub fn lrtup(&self) -> u16 {
         self.lrtup
     }
 
     /// Unambiguous Range, Interval Size
+    #[must_use]
     pub fn unambiguous_range(&self) -> u16 {
         self.unambiguous_range
     }
 
+    #[must_use]
     pub fn noise_level_horz(&self) -> f32 {
         self.noise_level_horz
     }
 
+    #[must_use]
     pub fn noise_level_vert(&self) -> f32 {
         self.noise_level_vert
     }
 
+    #[must_use]
     pub fn nyquist_velocity(&self) -> u16 {
         self.nyquist_velocity
     }
 
+    #[must_use]
     pub fn radial_flags(&self) -> u16 {
         self.radial_flags
     }
 
+    #[must_use]
     pub fn calib_const_horz_chan(&self) -> f32 {
         self.calib_const_horz_chan
     }
 
+    #[must_use]
     pub fn calib_const_vert_chan(&self) -> f32 {
         self.calib_const_vert_chan
     }
@@ -595,10 +668,12 @@ impl DataMoment {
         }
     }
 
+    #[must_use]
     pub fn data(&self) -> &GenericData {
         &self.data
     }
 
+    #[must_use]
     pub fn moment_data(&self) -> &[u8] {
         &self.moment_data
     }
@@ -622,64 +697,77 @@ pub struct GenericData {
 }
 
 impl GenericData {
+    #[must_use]
     pub fn data_block_type(&self) -> &[u8; 1] {
         &self.data_block_type
     }
 
+    #[must_use]
     pub fn data_name(&self) -> &[u8; 3] {
         &self.data_name
     }
 
+    #[must_use]
     pub fn reserved(&self) -> u32 {
         self.reserved
     }
 
     /// Number of data moment gates for current radial
+    #[must_use]
     pub fn number_data_moment_gates(&self) -> u16 {
         self.number_data_moment_gates
     }
 
     /// Range to center of first range gate
+    #[must_use]
     pub fn data_moment_range(&self) -> u16 {
         self.data_moment_range
     }
 
     /// Size of data moment sample interval
+    #[must_use]
     pub fn data_moment_range_sample_interval(&self) -> u16 {
         self.data_moment_range_sample_interval
     }
 
     /// Threshold parameter which specifies the minimum difference in echo power between two
     /// resolution gates for them not to be labeled "overlayed"
+    #[must_use]
     pub fn tover(&self) -> u16 {
         self.tover
     }
 
     /// SNR threshold for valid data
+    #[must_use]
     pub fn snr_threshold(&self) -> u16 {
         self.snr_threshold
     }
 
     /// Indicates special control features
+    #[must_use]
     pub fn control_flags(&self) -> u8 {
         self.control_flags
     }
 
     /// Number of bits (DWS) used for storing data for each Data Moment gate
+    #[must_use]
     pub fn data_word_size(&self) -> u8 {
         self.data_word_size
     }
 
     /// Scale value used to convert Data Moments from integer to floating point data
+    #[must_use]
     pub fn scale(&self) -> f32 {
         self.scale
     }
 
     /// Offset value used to convert Data Moments from integer to floating point data
+    #[must_use]
     pub fn offset(&self) -> f32 {
         self.offset
     }
 
+    #[must_use]
     pub fn moment_size(&self) -> usize {
         self.number_data_moment_gates as usize * self.data_word_size as usize / 8
     }
